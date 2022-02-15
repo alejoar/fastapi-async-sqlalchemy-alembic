722973d35f35586907b792679399901f670f14d3
Alejo Arias <alejo@reinventing.co>
2022-02-15 17:31:21 +0100

    remove anything async

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,6 @@
-import asyncio
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
-from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
 
@@ -19,7 +17,6 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadat
 from database import Base
-from models import ExampleModel
 
 target_metadata = Base.metadata
 
@@ -58,28 +55,6 @@ def do_run_migrations(connection):
 
     with context.begin_transaction():
         context.run_migrations()
-
-
-async def async_run_migrations_online():
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = AsyncEngine(
-        engine_from_config(
-            config.get_section(config.config_ini_section),
-            prefix="sqlalchemy.",
-            poolclass=pool.NullPool,
-            future=True,
-        )
-    )
-
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
-
-    await connectable.dispose()
 
 
 def run_migrations_online():
